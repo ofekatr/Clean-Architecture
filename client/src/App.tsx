bdb96b22ff23792c08fde2d689ac90ebcadc55ac
Ofek Atar <ofekatar98@gmail.com>
2021-05-14 00:34:20 +0300

    Extracted refresh token call to a hook.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import PageLoader from "./components/PageLoader";
+import useRefreshToken from "./hooks/useRefreshToken";
 import Router from "./Routes";
 
 function App() {
+  const loading = useRefreshToken();
+
+  if (loading) {
+    return <PageLoader />;
+  }
+
   return <Router />;
 }
 
